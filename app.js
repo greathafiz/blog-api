5e317415ed72469f1b065aacd84340a4fb0b3a6e
Abdulhafiz <abdulhafizaderemi@gmail.com>
2023-10-25 12:32:05 +0100

    security packages addede

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // import client from "./db/connect.js"
 import "dotenv/config";
-import express from "express";
 import "express-async-errors";
 import connectDB from "./db/connect.js";
 import { authRouter } from "./routers/authRouter.js";
@@ -8,12 +7,28 @@ import { articleRouter } from "./routers/articleRouter.js";
 import { commentRouter } from "./routers/commentRouter.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
+
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+
+import express from "express";
 const app = express();
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(helmet());
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => res.send(`<h3>My Blogging Platform</h3>`));
+app.get("/", (req, res) => res.send(`<h2>My Blogging Platform</h2>`));
 
 app.use("/api/v1", authRouter);
 app.use("/api/v1", articleRouter);
